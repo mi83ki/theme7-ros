da495be4fdfa95404d5ebcb38610b5d98a183e45
q-pon <miyamoto.kouta.3328@gmail.com>
2019-12-14 14:06:58 +0900

    publish不具合を修正

--- 01_RaspberryPi/odm_cal/src/odm_cal.cpp
+++ 01_RaspberryPi/odm_cal/src/odm_cal.cpp
@@ -1,6 +1,8 @@
 //#include "odm_cal.h"
 
 #include "ros/ros.h"
+#include "std_msgs/Int32.h"
+#include "std_msgs/UInt32.h"
 #include "nav_msgs/Odometry.h"
 #include "bits/stdc++.h"
 
@@ -15,189 +17,214 @@
 #define DEBUG_SUBSC
 
 typedef struct{
-    double x;
-    double y;
-    double th;
+    struct{
+        struct{
+            struct{
+                float_t x;
+                float_t y;
+                float_t z;
+            }position;
 
-    double v;
-    double omega;
-    //float x;
-    //float y;
-    //float th;
+            float_t th;
+        }pose;
+    }pose;
 
-    //float v;
-    //float omega;
-}stateType;
+    struct{
+        struct{
+            struct{
+                float_t vx;
+                float_t vy;
+                float_t vz;
+            }linear;
+
+            struct{
+                float_t omega_x;
+                float_t omega_y;
+                float_t omega_z;
+            }angular;
+        }twist;
+    }twist;
+}odmType;
 
 typedef struct{
-public:
-    double enc_L;
-    double enc_R;
-    //float enc_L;
-    //float enc_R;
+    int32_t L;
+    int32_t R;
 }encType;
 
 class OdmPublisher
 {
 public:
+    OdmPublisher():time(0),enc(),delta_enc(),agv_vel(0),odm_state()
+    {}
+
   void CalOdm();
-  //void ~CalOdm();
+//  void ~CalOdm();
   void InitOdm();
 
-  ros::Subscriber enc_sub;
-  ros::Subscriber enc_time_sub;
-
 protected:
-  //ros::Subscriber sub_gazebo;   ///<gazebo_msgs入力ハンドラ
+  void EncL_Callback(const std_msgs::Int32Ptr& encL_msg);
+  void EncR_Callback(const std_msgs::Int32Ptr& encR_msg);
+  void Time_Callback(const std_msgs::UInt32Ptr& time_msg);
+
+  ros::Time         cuurent_time;
+  ros::Publisher    odm_pub;
+
+  ros::Subscriber   encL_sub;
+  ros::Subscriber   encR_sub;
+  ros::Subscriber   enc_time_sub;
+
+  //ros::Subscriber sub_gazebo;   ///<gazebo_msgs入力ハンドラdelta_enc
   //ros::Publisher pub_pose;      ///<ロボット位置Pose 発行ハンドラ
 
-  ros::Time cuurent_time;
-  ros::Publisher odm_pub;
+  uint32_t time;
 
-
-#ifdef DEBUG
-  double enc_L;
-  double enc_R;
-
-  double pre_enc_L;
-  double pre_enc_R;
-
-  double time;
-  double pre_time;
-
-#else
-
-#endif
-
-  stateType state;
-  stateType pre_state;
+  encType   enc;
   encType   delta_enc;
+  float_t   agv_vel;
+  odmType   odm_state;
+
+  static uint32_t pre_time;
+
+  static encType   pre_enc;
+  static float_t   pre_agv_vel;
+  static odmType   pre_odm_state;
 };
+
+//int32_t OdmPublisher::enc_L;
+//int32_t OdmPublisher::enc_R;
+//uint32_t OdmPublisher::time;
+
+
+int32_t enc_L,enc_R;
+uint32_t OdmPublisher::pre_time;
+encType OdmPublisher::pre_enc;
+float_t OdmPublisher::pre_agv_vel;
+odmType OdmPublisher::pre_odm_state;
 
 void OdmPublisher::InitOdm()
 {
-#ifdef DEBUG
-//  enc_L = 1311;
-//  enc_R = 1311;     /* 310.41 */
-
-  enc_L = 1000;
-//  enc_R = 1894;     /* 310.41 */
-  enc_R = 1010;
-
-  pre_enc_L = 1000;
-  pre_enc_R = 1000;
-
-  time      = 100010;
-  pre_time  = 100000;
-
-#else
-
-#endif
-  ros::NodeHandle n;
-  odm_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
-  ROS_INFO("[odm_cal] init");
-
+    pre_time = 0;
+    pre_agv_vel = 0.0;
+    encType pre_enc = {0};
+    odmType pre_odm_state = {0.0};
 }
 
 void OdmPublisher::CalOdm()
 {
-  cuurent_time = ros::Time::now();
+  ROS_INFO("[odm_cal] init");
+  ros::NodeHandle n;
 
-#ifdef DEBUG
-  InitOdm();
-#endif
+  encL_sub = n.subscribe("A2_encL", 50, &OdmPublisher::EncL_Callback, this);
+  encR_sub = n.subscribe("A2_encR", 50, &OdmPublisher::EncR_Callback, this);
+  enc_time_sub = n.subscribe("A2_time", 50, &OdmPublisher::Time_Callback, this);
+  odm_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
 
-  /* エンコーダ加算値とサンプリング時間を計算 */
-  delta_enc.enc_L = enc_L - pre_enc_L;
-  delta_enc.enc_R = enc_R - pre_enc_R;
-  double delta_t = (time - pre_time) * 0.001;
+  ROS_INFO("[odm_cal] START");
 
-  pre_time = time;
-
-  /* 車輪の速度を計算 */
-  double wheel_v_L = (2 *M_PI *WHEEL_R *delta_enc.enc_L) / (RATE_ENC *RATE_REDC *delta_t);
-  double wheel_v_R = (2 *M_PI *WHEEL_R *delta_enc.enc_R) / (RATE_ENC *RATE_REDC *delta_t);
-
-  /* 車体の速度と回転角速度を計算 */
-  state.v = (wheel_v_L + wheel_v_R)/2;
-  state.omega = (wheel_v_R - wheel_v_L)/(WHEEL_TREAD); //WHEEL_Tトレッド
-
-  /* 位置座標を計算 */
-  double delta_th = delta_t * (state.omega + pre_state.omega)/2;
-  state.th += delta_th;
-
-  double delta_x = delta_t * (state.v * cos(state.th) + pre_state.v * cos(pre_state.th))/2;
-  double delta_y = delta_t * (state.v * sin(state.th) + pre_state.v * sin(pre_state.th))/2;
-
-  state.x += delta_x;
-  state.y += delta_y;
-
-  ROS_INFO("[odm_cal] dt = %lf, encL = %lf, encR = %lf", delta_t, delta_enc.enc_L, delta_enc.enc_R);
-
-  ROS_INFO("[odm_cal] v = %lf, omega = %lf", state.v, state.omega);
-
-  ROS_INFO("[odm_cal] (dx, dy, dth) = (%lf, %lf ,%lf)", delta_x, delta_y, delta_th);
-  ROS_INFO("[odm_cal] (x, y, th)    = (%lf, %lf ,%lf)", state.x, state.y, state.th);
-
-  nav_msgs::Odometry odm;
-
-//  odm.header.stamp = cuurent_time;
-//  odm.header.frame_id = "odm";
-
-  odm.pose.pose.position.x = state.x;
-  odm.pose.pose.position.y = state.y;
-  odm.pose.pose.position.z = 0.0;
-  //odm.pose.pose.orientation.w = state.th;
-
-  odm.twist.twist.linear.x = state.v * cos(state.th);
-  odm.twist.twist.linear.y = state.v * sin(state.th);
-  odm.twist.twist.linear.z = 0.0;
-
-  odm.twist.twist.angular.x = 0.0;
-  odm.twist.twist.angular.y = 0.0;
-  odm.twist.twist.angular.z = state.omega;
-
-  odm_pub.publish(odm);
-
-  pre_state = state;
 }
 
 /*
 void OdmPublisher::~CalOdm()
 {
 
-}  ros::Publisher OdmPub.odm_pub = n.advertise<nav_msgs::Odometry>("odm", 50);
-
+}
 */
+
+void OdmPublisher::EncL_Callback(const std_msgs::Int32Ptr& encL_msg)
+{
+    enc.L = encL_msg->data;
+//    ROS_INFO("pub enc.L %d", enc_L);
+}
+
+void OdmPublisher::EncR_Callback(const std_msgs::Int32Ptr& encR_msg)
+{
+    enc.R = encR_msg->data;
+//    ROS_INFO("pub enc.R %d", enc.R);
+}
+
+void OdmPublisher::Time_Callback(const std_msgs::UInt32Ptr& time_msg)
+{
+  //ROS_INFO("pub time %d", time);
+
+  time = static_cast<double>(time_msg->data);
+
+  /* エンコーダ加算値とサンプリング時間を計算 */
+  if (time == pre_time) {
+      return;
+  }
+  ROS_INFO("[odm_cal] d_encL,R %d %d", delta_enc.L, delta_enc.R);
+
+  delta_enc.L = enc.L - pre_enc.L;
+  delta_enc.R = enc.R - pre_enc.R;
+
+  float_t delta_t = (time - pre_time) * 0.001;
+  ROS_INFO("[odm_cal] dt = %f", delta_t);
+
+  pre_enc.L = enc.L;
+  pre_enc.R = enc.R;
+  pre_time = time;
+
+  /* 車輪の速度を計算 */
+  float_t wheel_v_L = (float_t) (2 *M_PI *WHEEL_R *delta_enc.L) / (RATE_ENC *RATE_REDC *delta_t);
+  float_t wheel_v_R = (float_t) (2 *M_PI *WHEEL_R *delta_enc.R) / (RATE_ENC *RATE_REDC *delta_t);
+
+  /* 車体の速度と回転角速度を計算 */
+  agv_vel = (wheel_v_L + wheel_v_R)/2;
+
+  int32_t *agv_posi_ptr;
+  int32_t *agv_twist_ptr;
+
+  //agv_twist_ptr = &odm_state.twist.twist.angular;
+  odm_state.twist.twist.angular.omega_z = (wheel_v_R - wheel_v_L)/(WHEEL_TREAD); //WHEEL_Tトレッド
+  ROS_INFO("[odm_cal] agv_vel = %f, omega = %f", agv_vel, odm_state.twist.twist.angular.omega_z);
+
+  /* 位置座標を計算 */
+  float_t delta_th = (float_t) delta_t *(odm_state.twist.twist.angular.omega_z + pre_odm_state.twist.twist.angular.omega_z)/2;
+  odm_state.pose.pose.th += delta_th;
+  ROS_INFO("[odm_cal] d_th =  %f, th = %f", delta_th, odm_state.pose.pose.th);
+
+  odm_state.twist.twist.linear.vx = agv_vel *cos(odm_state.pose.pose.th);
+  odm_state.twist.twist.linear.vy = agv_vel *sin(odm_state.pose.pose.th);
+  ROS_INFO("[odm_cal] vx = %f, vy = %f", odm_state.twist.twist.linear.vx, odm_state.twist.twist.linear.vy);
+
+  float_t delta_x = (float_t) delta_t *(odm_state.twist.twist.linear.vx + pre_odm_state.twist.twist.linear.vx)/2;
+  float_t delta_y = (float_t) delta_t *(odm_state.twist.twist.linear.vy + pre_odm_state.twist.twist.linear.vy)/2;
+  ROS_INFO("[odm_cal] (dx, dy) = (%f, %f)",delta_x, delta_y);
+
+  odm_state.pose.pose.position.x += delta_x;
+  odm_state.pose.pose.position.y += delta_y;
+  ROS_INFO("[odm_cal] (x, y) = (%f, %f)",odm_state.pose.pose.position.x, odm_state.pose.pose.position.y);
+
+  nav_msgs::Odometry odm;
+
+//  odm.header.stamp = cuurent_time;
+//  odm.header.frame_id = "odm";
+  odm.pose.pose.position.x = odm_state.pose.pose.position.x;
+  odm.pose.pose.position.y = odm_state.pose.pose.position.y;
+  odm.pose.pose.position.z = 0;
+
+  odm.twist.twist.linear.x = odm_state.twist.twist.linear.vx;
+  odm.twist.twist.linear.y = odm_state.twist.twist.linear.vy;
+  odm.twist.twist.linear.z = 0.0;
+
+  odm.twist.twist.angular.x = 0.0;
+  odm.twist.twist.angular.y = 0.0;
+  odm.twist.twist.angular.z = odm_state.twist.twist.angular.omega_z;
+
+  odm_pub.publish(odm);
+
+  pre_odm_state = odm_state;
+  ROS_INFO("[odm_cal] =========================== ");
+}
 
 int main(int argc, char **argv)
 {
-  int cnt = 0;
-
   ros::init(argc, argv, "odm_cal");
-
-
   OdmPublisher OdmPub;
-  ros::NodeHandle n;
-
-  ros::Rate r(10);
 
   OdmPub.InitOdm();
-  ROS_INFO("[odm_cal] START");
-//  ros::Subscriber enc_sub = n.subscribe("enc", 50, OdmPub.CalOdm);
-//  ros::spin();
+  OdmPub.CalOdm();
 
- // /*
-  while (ros::ok())
-  {
-    OdmPub.CalOdm();
+  ros::spin();
 
-    ++cnt;
-    ROS_INFO("[odm_cal] %d", cnt);
-
-    ros::spinOnce();
-    r.sleep();
-  }
-  return 0;
- // */
 }
